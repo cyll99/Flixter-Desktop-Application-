d52d93e9cd9f49a70e5ef4580408f080f38751fc
cyll99 <christyanlove@gmail.com>
2022-10-07 22:49:22 -0700

    button on fillms form

--- Flixter/frmFilms.Designer.cs
+++ Flixter/frmFilms.Designer.cs
@@ -30,8 +30,10 @@ namespace Flixter
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_precedent = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,26 +46,46 @@ namespace Flixter
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox1
+            // lbl_title
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(12, 114);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(342, 40);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Location = new System.Drawing.Point(23, 114);
+            this.lbl_title.MinimumSize = new System.Drawing.Size(400, 30);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(400, 30);
+            this.lbl_title.TabIndex = 2;
+            this.lbl_title.Text = "label1";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // richTextBox1
+            // label1
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 170);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(342, 245);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 174);
+            this.label1.MinimumSize = new System.Drawing.Size(400, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 250);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // btn_precedent
+            // 
+            this.btn_precedent.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btn_precedent.Location = new System.Drawing.Point(26, 472);
+            this.btn_precedent.Name = "btn_precedent";
+            this.btn_precedent.Size = new System.Drawing.Size(75, 23);
+            this.btn_precedent.TabIndex = 4;
+            this.btn_precedent.Text = "Precedent";
+            this.btn_precedent.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Location = new System.Drawing.Point(542, 472);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Suivant";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // frmFilms
             // 
@@ -71,11 +93,14 @@ namespace Flixter
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(900, 600);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_precedent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_title);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmFilms";
+            this.Text = "Flixter";
             this.Load += new System.EventHandler(this.frmFilms_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_title_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -86,7 +111,9 @@ namespace Flixter
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_precedent;
+        private System.Windows.Forms.Button button1;
     }
 }
